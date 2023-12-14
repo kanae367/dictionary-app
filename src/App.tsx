74453ab84e03cd00ac47b58b4c219add20876629
”Kodoku” <kodokunawa@gmail.com>
2023-12-14 15:27:52 +0300

    Добавлена валидация

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import book from './assets/icon_book.svg';
 import moon from './assets/icon_moon.svg';
@@ -6,8 +6,10 @@ import link from './assets/icon_link.svg';
 import getWord from './getWord.ts';
 import WordMeaning from './WordMeaning.tsx';
 import Select from './select/Select.tsx';
+import { ErrorMessage } from '@hookform/error-message';
+import './NotFound.scss';
 import './darkTheme.scss';
-import './App.scss'
+import './App.scss';
 import './Main.scss';
 
 const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
@@ -38,15 +40,27 @@ interface IWordResult{
   sourceUrls: string[];
 }
 
+interface INotFoundResult{
+  title: string;
+  message: string;
+  resolution: string;
+}
+
 function App() {
-  const {register, handleSubmit} = useForm<IFormInput>();
+  const {register, formState:{errors}, handleSubmit} = useForm<IFormInput>();
   const [isDarkTheme, setIsDarkTheme] = useState<Boolean>(Boolean(darkThemeMq));
   const [word, setWord] = useState<false | IWordResult>(false);
+  const [error, setError] = useState<false | INotFoundResult>(false);
   const [font, setFont] = useState<'Inter' | 'Lora' | 'Mono'>('Mono');
 
   const onSubmit:SubmitHandler<IFormInput> = async (data) => {
-    const newWord:IWordResult[] = await getWord(data.word);
-    setWord(newWord[0]);
+    const result:IWordResult[] | INotFoundResult = await getWord(data.word);
+    if(Array.isArray(result)){
+      setWord(result[0]);
+    }else{
+      setWord(false);
+      setError(result);
+    }
   }
 
   const onThemeChange = () => {
@@ -84,28 +98,52 @@ function App() {
 
         <main className='main'>
             <form className='search-bar' onSubmit={handleSubmit(onSubmit)}>
-              <input type="text" className='search-bar__input' {...register("word", {required: true, minLength: 2})} placeholder='Search for any word...'/>
+              <ErrorMessage 
+                errors={errors}
+                name='word'
+                render={({message}) => <p className='error-message'>{message}</p>}
+                />
+              <input type="text" className='search-bar__input' {...register("word", {required: 'Whoops, can’t be empty…', minLength: 2})} placeholder='Search for any word...'/>
               <button type='submit' className='search-bar__button'/>
             </form>
             <div className='result'>
-              <div className='result__header'>
-                <div className='result__header-text'>
-                  <h2 className='result__title'>{word ? word.word : 'Nothing'}</h2>
-                  <p className='result__phonetic'>{word ? `/${word.word}/` : 'Nothing'}</p>
-                </div>
-                <button className='result__button'></button>
-              </div>
+              {
+                word &&
+                <>
+                  <div className='result__header'>
+                    <div className='result__header-text'>
+                      <h2 className='result__title'>{word.word}</h2>
+                      <p className='result__phonetic'>{word.phonetic}</p>
+                    </div>
+                    <button className='result__button'></button>
+                  </div>
 
-              <div className='result__content'>
-                {items}
-              </div>
+                  <div className='result__content'>
+                    {items}
+                  </div>
 
-              <div className='result__sources'>
-                <h6 className='result__sources-title'>Source</h6>
-                <div className='result__sources-links'>
-                  {sources}
+                  <div className='result__sources'>
+                    <h6 className='result__sources-title'>Source</h6>
+                    <div className='result__sources-links'>
+                      {sources}
+                    </div>
+                  </div>
+                </>
+              }
+
+              {
+                error &&
+                <div className='not-found'>
+                  <h3 className='not-found__header'>
+                    <span className='not-found__emoji'>😕</span>
+                    <span className='not-found__subheader'>{error.title}</span>
+                  </h3>
+                  <p className='not-found__text'>
+                    {`${error.message} ${error.resolution}`}
+                  </p>
                 </div>
-              </div>
+              }
+              
             </div>
         </main>
       </div>
